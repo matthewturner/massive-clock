6931b90debe91fbac55c7e437ebd45acaedc98a9
Matt Turner <matt@fiverings.uk>
2021-08-26 08:53:41 +0100

    Divide strip into segments

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,137 @@
 #include "main.h"
 
-Dependency dependency;
-
 void setup()
 {
   Serial.begin(9600);
 
+  FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, BGR>(physicalLeds, NUM_LEDS);
+  FastLED.setBrightness(10);
+
   Serial.println("Setup complete. Continuing...");
 }
 
+byte counter = 0;
+
 void loop()
 {
-  if (dependency.method())
+  setDigit(0, counter);
+  setDigit(1, counter);
+  setDigit(2, counter);
+  setDigit(3, counter);
+  showDigits();
+  counter++;
+  if (counter >= 10)
   {
-    Serial.println("Was true");
+    counter = 0;
   }
-}
-
-#include <FastLED.h>
-
-#define NUM_LEDS 60
-
-#define DATA_PIN 3
-#define CLOCK_PIN 13
-
-CRGB leds[NUM_LEDS];
-
-void setup()
-{
-  FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, BGR>(leds, NUM_LEDS);
-  FastLED.setBrightness(10);
-}
-
-void loop()
-{
-  setDigit(0, 0);
-  FastLED.show();
-  delay(30);
+  delay(100);
 }
 
 void setDigit(byte digit, byte value)
 {
+  setIndividualDigit(value);
+  applyDigit(digit);
+}
+
+void applyDigit(byte digit)
+{
+  byte offset = LEDS_PER_DIGIT * digit;
+  if (digit > 1)
+  {
+    offset += LEDS_IN_SEPARATOR;
+  }
+  for (byte i = 0; i < LEDS_PER_DIGIT; i++)
+  {
+    leds[i + offset] = segments[i];
+  }
+}
+
+void setIndividualDigit(byte value)
+{
+  showAllSegments();
+
   switch (value)
   {
   case 0:
-    leds[digit + 0] = CRGB::Blue;
-    leds[digit + 1] = CRGB::Blue;
-    leds[digit + 2] = CRGB::Blue;
-    leds[digit + 3] = CRGB::Blue;
-    leds[digit + 4] = CRGB::Blue;
-    leds[digit + 5] = CRGB::Blue;
-    leds[digit + 8] = CRGB::Blue;
-    leds[digit + 9] = CRGB::Blue;
-    leds[digit + 10] = CRGB::Blue;
-    leds[digit + 11] = CRGB::Blue;
-    leds[digit + 12] = CRGB::Blue;
-    leds[digit + 13] = CRGB::Blue;
+    hideSegment(SEG_MIDDLE);
+    break;
+  case 1:
+    hideSegment(SEG_TOP_LEFT);
+    hideSegment(SEG_TOP);
+    hideSegment(SEG_MIDDLE);
+    hideSegment(SEG_BOTTOM_LEFT);
+    hideSegment(SEG_BOTTOM);
+    break;
+  case 2:
+    hideSegment(SEG_TOP_LEFT);
+    hideSegment(SEG_BOTTOM_RIGHT);
+    break;
+  case 3:
+    hideSegment(SEG_TOP_LEFT);
+    hideSegment(SEG_BOTTOM_LEFT);
+    break;
+  case 4:
+    hideSegment(SEG_TOP);
+    hideSegment(SEG_TOP_RIGHT);
+    hideSegment(SEG_BOTTOM_LEFT);
+    hideSegment(SEG_BOTTOM);
+    break;
+  case 5:
+    hideSegment(SEG_TOP_RIGHT);
+    hideSegment(SEG_BOTTOM_LEFT);
+    break;
+  case 6:
+    hideSegment(SEG_TOP_RIGHT);
+    break;
+  case 7:
+    hideSegment(SEG_TOP_LEFT);
+    hideSegment(SEG_BOTTOM_LEFT);
+    hideSegment(SEG_BOTTOM);
+    break;
+  case 9:
+    hideSegment(SEG_BOTTOM_LEFT);
     break;
   }
+}
+
+void showAllSegments()
+{
+  for (byte i = 0; i < LEDS_PER_DIGIT; i++)
+  {
+    segments[i] = true;
+  }
+}
+
+void showSegment(byte segment)
+{
+  setSegment(segment, true);
+}
+
+void hideSegment(byte segment)
+{
+  setSegment(segment, false);
+}
+
+void setSegment(byte segment, bool show)
+{
+  for (byte i = 0; i < LEDS_PER_SEGMENT; i++)
+  {
+    segments[segment + i] = show;
+  }
+}
+
+void showDigits()
+{
+  for (byte i = 0; i < NUM_LEDS; i++)
+  {
+    if (leds[i])
+    {
+      physicalLeds[i] = CRGB::Purple;
+    }
+    else
+    {
+      physicalLeds[i] = CRGB::Black;
+    }
+  }
+  FastLED.show();
 }
