be0ff93518407f5b479d7578fcf54ffe74aaaa51
Matt Turner <matt@fiverings.uk>
2021-08-27 11:24:07 +0100

    Start on display

--- lib/Display/Display.cpp
+++ lib/Display/Display.cpp
@@ -7,117 +7,36 @@ bool Display::led(byte led)
 
 void Display::clear()
 {
-  bufferAllSegments(false);
-  for (byte i = 0; i < NUM_LEDS; i++)
+  for (byte i = 0; i < 4; i++)
   {
-    _leds[i] = false;
+    _digits[i].clear();
   }
 }
 
 void Display::setDigit(byte digit, byte value)
 {
-  bufferDigit(value);
-  applyDigit(digit);
+  _digits[digit].set(value);
 }
 
 void Display::setPart(byte part, byte value)
 {
   byte offset = part * 2;
-  bufferDigit(value % 10);
-  applyDigit(0 + offset);
-  bufferDigit((value - (value % 10)) / 10);
-  applyDigit(1 + offset);
+  setDigit(0 + offset, value % 10);
+  bufferDigit(1 + offset, (value - (value % 10)) / 10);
 }
 
-void Display::applyDigit(byte digit)
-{
-  byte offset = LEDS_PER_DIGIT * digit;
-  if (digit > 1)
-  {
-    offset += LEDS_IN_SEPARATOR;
-  }
-  for (byte s = 0; s < SEGMENTS_PER_DIGIT; s++)
-  {
-    for (byte i = 0; i < LEDS_PER_SEGMENT; i++)
-    {
-      _leds[i + (s * LEDS_PER_SEGMENT) + offset] = _segments[s];
-    }
-  }
-}
+// void Display::setSegmentRange(byte first, byte last, bool show)
+// {
+//   for (byte i = first; i <= last; i++)
+//   {
+//     _segments[i] = show;
+//   }
+// }
 
-void Display::bufferDigit(byte value)
-{
-  bufferAllSegments(true);
-
-  switch (value)
-  {
-  case 0:
-    bufferSegment(SEG_MIDDLE, false);
-    break;
-  case 1:
-    bufferSegment(SEG_TOP_LEFT, false);
-    bufferSegment(SEG_TOP, false);
-    bufferSegment(SEG_MIDDLE, false);
-    bufferSegment(SEG_BOTTOM_LEFT, false);
-    bufferSegment(SEG_BOTTOM, false);
-    break;
-  case 2:
-    bufferSegment(SEG_TOP_LEFT, false);
-    bufferSegment(SEG_BOTTOM_RIGHT, false);
-    break;
-  case 3:
-    bufferSegment(SEG_TOP_LEFT, false);
-    bufferSegment(SEG_BOTTOM_LEFT, false);
-    break;
-  case 4:
-    bufferSegment(SEG_TOP, false);
-    bufferSegment(SEG_TOP_RIGHT, false);
-    bufferSegment(SEG_BOTTOM_LEFT, false);
-    bufferSegment(SEG_BOTTOM, false);
-    break;
-  case 5:
-    bufferSegment(SEG_TOP_RIGHT, false);
-    bufferSegment(SEG_BOTTOM_LEFT, false);
-    break;
-  case 6:
-    bufferSegment(SEG_TOP_RIGHT, false);
-    break;
-  case 7:
-    bufferSegment(SEG_TOP_LEFT, false);
-    bufferSegment(SEG_BOTTOM_LEFT, false);
-    bufferSegment(SEG_BOTTOM, false);
-    break;
-  case 9:
-    bufferSegment(SEG_BOTTOM_LEFT, false);
-    break;
-  }
-}
-
-void Display::bufferAllSegments(bool show)
-{
-  for (byte i = 0; i < SEGMENTS_PER_DIGIT; i++)
-  {
-    _segments[i] = show;
-  }
-}
-
-void Display::bufferSegment(byte segment, bool show)
-{
-  _segments[segment] = show;
-}
-
-void Display::setSegmentRange(byte first, byte last, bool show)
-{
-  for (byte i = first; i <= last; i++)
-  {
-    _segments[i] = show;
-  }
-}
-
-void Display::setLedRange(byte first, byte last, bool show)
-{
-  for (byte i = first; i <= last; i++)
-  {
-    _leds[i] = show;
-  }
-}
+// void Display::setLedRange(byte first, byte last, bool show)
+// {
+//   for (byte i = first; i <= last; i++)
+//   {
+//     _leds[i] = show;
+//   }
+// }
--- lib/Display/Display.h
+++ lib/Display/Display.h
@@ -1,40 +1,29 @@
 #ifndef Digit_h
 #define Digit_h
 
-#include <stdint.h>
-typedef uint8_t byte;
+#include "common.h"
+#include "Digit.h"
+#include "Separator.h"
 
 #define NUM_LEDS 60
-const byte LEDS_PER_SEGMENT = 2;
-const byte SEGMENTS_PER_DIGIT = 7;
-const byte LEDS_IN_SEPARATOR = 2;
-const byte LEDS_PER_DIGIT = LEDS_PER_SEGMENT * SEGMENTS_PER_DIGIT;
-
-const byte SEG_TOP_LEFT = 0;
-const byte SEG_TOP = 1;
-const byte SEG_TOP_RIGHT = 2;
-const byte SEG_MIDDLE = 3;
-const byte SEG_BOTTOM_LEFT = 4;
-const byte SEG_BOTTOM = 5;
-const byte SEG_BOTTOM_RIGHT = 6;
 
 class Display
 {
 public:
     bool led(byte led);
     void setDigit(byte digit, byte value);
-    void setPart(byte part, byte value);
-    void bufferDigit(byte value);
-    void applyDigit(byte digit);
-    void bufferSegment(byte segment, bool show);
-    void bufferAllSegments(bool show);
-    void setSegmentRange(byte first, byte last, bool show);
-    void setLedRange(byte first, byte last, bool show);
+    // void setPart(byte part, byte value);
+    // void setSegment(byte segment, bool show);
+    // void setSegmentRange(byte first, byte last, bool show);
+    // void setLedRange(byte first, byte last, bool show);
     void clear();
+    void setAll();
+    void hide();
+    void show();
 
 private:
-    bool _segments[SEGMENTS_PER_DIGIT];
-    bool _leds[NUM_LEDS];
+    Digit _digits[4];
+    Separator _separator;
 };
 
 #endif
