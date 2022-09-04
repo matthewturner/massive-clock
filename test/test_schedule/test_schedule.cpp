aff90a80a083eda13689a33771e9eb0b1c33b70d
Matt Turner <matt@fiverings.uk>
2022-09-04 21:25:41 +0100

    Nearly there

--- test/test_schedule/test_schedule.cpp
+++ test/test_schedule/test_schedule.cpp
@@ -46,10 +46,12 @@ void test_segment_initializes(void)
 
 void test_segment_setup_range(void)
 {
-    segmentTarget.setup(5, 6, 12);
+    byte startSegment = 5 * SEGMENTS_IN_HOUR;           // 5.00am
+    byte endSegment = ((6 + 1) * SEGMENTS_IN_HOUR) - 1; // 6.50am
+    segmentTarget.setup(startSegment, endSegment, 12);
     for (byte i = 0; i < SEGMENTS_IN_DAY; i++)
     {
-        if (i == 5 || i == 6)
+        if (i >= startSegment && i <= endSegment)
         {
             TEST_ASSERT_EQUAL(12, segmentTarget.valueFor(i));
         }
@@ -66,7 +68,7 @@ int main(int argc, char **argv)
     RUN_TEST(test_hour_initializes);
     RUN_TEST(test_hour_setup_range);
     RUN_TEST(test_segment_initializes);
-    // RUN_TEST(test_segment_setup_range);
+    RUN_TEST(test_segment_setup_range);
     UNITY_END();
 
     return 0;
