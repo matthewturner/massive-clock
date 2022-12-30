3ac608886c7261e24250d91e7c745d59c223a3ac
Matt Turner <matt@fiverings.uk>
2022-12-30 13:24:21 +0000

    Revert

--- src/main.cpp
+++ src/main.cpp
@@ -47,7 +47,7 @@ bool idle()
   Serial.flush();
   LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF,
                 SPI_OFF, USART0_OFF, TWI_OFF);
-  return false;
+  return true;
 }
 
 bool show()
